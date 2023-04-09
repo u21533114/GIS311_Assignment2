1aadf9b0f4bec144c9b574322426eadfdc83bab9
u21533114 <130218490+u21533114@users.noreply.github.com>
2023-04-09 12:50:02 +0200

    Update GIS311_Assignment2.py

--- GIS311_Assignment2.py
+++ GIS311_Assignment2.py
@@ -4,11 +4,12 @@ import math
 import pandas as pd
 import streamlit as st
 import numpy as np
-#airlines is a table
-#airports has co-ordinates and no header row
+import geopandas as gpd
+#airlines is a table --> can use SQL to create an attribute join to countries table
+#airports has co-ordinates --> can create points and plot these on a map; can also join to countries table
 #countries is a table
-#planes is a table with no header row
-#routes is weird and has no header row
+#planes is a table --> can create a bar chart for types of planes
+#routes is weird
 #NB: 1 map and two charts are needed
 
 
@@ -18,12 +19,7 @@ This data was aquired from [OpenFlights](https://openflights.org/data.html)
 """
 
 airlines = pd.read_csv('airlines.dat', header = None)
-airlines
 airports = pd.read_csv('airports.dat', header = None)
-airports
 countries = pd.read_csv('countries.dat', header = None)
-countries
 planes = pd.read_csv('planes.dat', header = None)
-planes
 routes = pd.read_csv('routes.dat', header = None)
-routes
