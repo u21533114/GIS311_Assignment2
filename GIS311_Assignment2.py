c63e023e92d104982a3e18e8c23e8190a203e5b1
u21533114 <130218490+u21533114@users.noreply.github.com>
2023-04-09 12:35:20 +0200

    Update GIS311_Assignment2.py

--- GIS311_Assignment2.py
+++ GIS311_Assignment2.py
@@ -12,16 +12,30 @@ import numpy as np
 #routes is weird and has no header row
 #NB: 1 map and two charts are needed
 
-""" # Flight Data Anaylsis \U00002708
-This data was aquired from [OpenFlights](https://openflights.org/data.html) """
+st.markdown(
+    """
+    <style>
+    .reportview-container {
+        padding-top: 0px;
+        padding-bottom: 0px;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
+"""
+# Flight Data Anaylsis \U00002708
+This data was aquired from [OpenFlights](https://openflights.org/data.html)
+"""
 
 airlines = pd.read_csv('airlines.dat')
 airlines
-airports = pd.read_csv('airports.dat')
+airports = pd.read_csv('airports.dat', header = None)
 airports
 countries = pd.read_csv('countries.dat')
 countries
-planes = pd.read_csv('planes.dat')
+planes = pd.read_csv('planes.dat', header = None)
 planes
-routes = pd.read_csv('routes.dat')
+routes = pd.read_csv('routes.dat', header = None)
 routes
