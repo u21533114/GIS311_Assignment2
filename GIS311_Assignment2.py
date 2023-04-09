220407e2bbf1546e535aa66b15eed8e17eefe712
u21533114 <130218490+u21533114@users.noreply.github.com>
2023-04-09 11:45:15 +0200

    Update GIS311_Assignment2.py

--- GIS311_Assignment2.py
+++ GIS311_Assignment2.py
@@ -1,5 +1,8 @@
 #add python code here
+git clone https://github.com/u21533114/GIS311_Assignment2.git
 import streamlit as st
 import pandas as pd
 import numpy as np
 print('hi')
+git commit -m "Initial commit"
+git push origin main
