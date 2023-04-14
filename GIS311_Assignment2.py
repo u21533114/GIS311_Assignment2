469b68fd1df747de150ab266f37fef2e756f1f85
u21533114 <130218490+u21533114@users.noreply.github.com>
2023-04-14 15:20:34 +0200

    Update GIS311_Assignment2.py

--- GIS311_Assignment2.py
+++ GIS311_Assignment2.py
@@ -54,28 +54,23 @@ city_dict = {'JNB': 'Johannesburg',
 major_sa_routes['Source City'] = major_sa_routes['Source IATA'].apply(lambda x: city_dict.get(x, 'Foreign'))
 major_sa_routes['Destination City'] = major_sa_routes['Destination IATA'].apply(lambda x: city_dict.get(x, 'Foreign'))
 
-# count values
+#count values
 source_counts = major_sa_routes['Source City'].value_counts()
 dest_counts = major_sa_routes['Destination City'].value_counts()
 total_counts = source_counts + dest_counts
 total_counts.drop('Foreign', inplace=True)
+#total_counts['Airport'] = ['OR Tambo International Airport', 'Cape Town International Airport', 'King Shaka International Airport', 'Port Elizabeth Airport', 'East London Airport', 'George Airport', 'Bram Fischer Airport', 'Lanseria Airport']
 
-# Add airport names to the dataframe
-total_counts['Airport'] = ['OR Tambo International Airport', 'Cape Town International Airport',
-                           'King Shaka International Airport', 'Port Elizabeth Airport', 
-                           'East London Airport', 'George Airport', 'Bram Fischer Airport', 
-                           'Lanseria Airport']
-
-# plot bar chart
+#plot bar chart
 total_counts = total_counts.reset_index()
-total_counts.columns = ['City', 'Count', 'Airport']
+total_counts.columns = ['City', 'Count']
 
 chart = alt.Chart(total_counts).mark_bar().encode(
-    x=alt.X('Airport:N', axis=alt.Axis(title='Airport', labelAngle=0)),
+    x=alt.X('City:N', axis=alt.Axis(title='City', labelAngle=0)),
     y=alt.Y('Count:Q', axis=alt.Axis(title='Count')),
-    tooltip=[alt.Tooltip('Airport:N', title='Airport'), alt.Tooltip('Count:Q', title='Count')]
+    tooltip=[alt.Tooltip('City:N', title='City'), alt.Tooltip('Count:Q', title='Count')]
 ).properties(
-    title='Number of airline destinations from each airport',
+    title='Number of airline destinations from each city',
     width=600,
     height=400
 )
